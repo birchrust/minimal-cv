a41448933eda87cd613f6bacadaf0ffb43709da2
birch <121175143+birchrust@users.noreply.github.com>
2023-07-26 18:31:31 +0800

    feate: update

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -11,7 +11,7 @@ const Tiptap = ({ content }: { content: string }) => {
           "prose prose-sm sm:prose lg:prose-lg m-6 focus:outline-none space-y-4",
       },
     },
-    content: content == "" ? "t" : content,
+    content: content == "" ? "Visit 'https://iliyazelenko.github.io/tiptap-vuetify-demo/#/preview' to generate HTML tags" : content,
   });
 
   return <EditorContent editor={editor} />;
