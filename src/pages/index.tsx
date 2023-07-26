c4a7d1a35ce550fd0ace77b927df6c9aae18e39a
birch <121175143+birchrust@users.noreply.github.com>
2023-07-26 18:28:39 +0800

    chore: test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Head from "next/head";
 import Tiptap from "~/components/Tiptap";
-import config from "../../public/config.json";
 
-const author = config.config[0]?.options[0]?.value;
-const des = config.config[0]?.options[0]?.value;
-const title = config.config[0]?.options[0]?.value;
+import React from "react";
 
-export default function Home() {
+interface Content {
+  config: {
+    name: string;
+    options: {
+      type: string;
+      key: string;
+      value: string;
+      placeholder: string;
+    }[];
+  }[];
+}
+
+export default function Home(props: Content) {
+  const author = props.config[0]?.options[0]?.value;
+  const des = props.config[1]?.options[0]?.value;
+  const title = props.config[2]?.options[0]?.value;
+  const content = props.config[3]?.options[0]?.value;
+
   return (
     <>
       <Head>
@@ -25,7 +40,7 @@ export default function Home() {
         <div className="my-0 bg-stone-50 dark:bg-slate-900 sm:my-14">
           <div id="main" className="container mx-auto flex justify-center">
             <div className="min-w-[815px] rounded border bg-white p-10 shadow">
-              <Tiptap />
+              <Tiptap content={content!} />
             </div>
           </div>
         </div>
@@ -34,8 +49,14 @@ export default function Home() {
   );
 }
 
-export function getStaticProps() {
+import fsPromises from "fs/promises";
+import path from "path";
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "/public/config.json");
+  const jsonData = await fsPromises.readFile(filePath, "utf-8");
+  const objectData = JSON.parse(jsonData);
+
   return {
-    props: {},
+    props: objectData,
   };
 }
